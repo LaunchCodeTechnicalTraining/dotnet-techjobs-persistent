a167e0713023efa4c9efd57c00cbd35844306cb5
Sally Steuterman <sally.steuterman@gmail.com>
2020-07-06 17:25:16 -0500

    Added List and Search functionality

--- TechJobsPersistent/Controllers/SearchController.cs
+++ TechJobsPersistent/Controllers/SearchController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.Data;
+using Microsoft.EntityFrameworkCore;
+using TechJobsPersistent.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,28 +14,90 @@ namespace TechJobsPersistent.Controllers
 {
     public class SearchController : Controller
     {
+        private JobDbContext context;
+
+        public SearchController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            // ViewBag.columns = ListController.ColumnChoices;
+            ViewBag.columns = ListController.ColumnChoices;
             return View();
         }
 
         public IActionResult Results(string searchType, string searchTerm)
         {
             List<Job> jobs;
+            List<JobDetailViewModel> displayJobs = new List<JobDetailViewModel>();
+
             if (string.IsNullOrEmpty(searchTerm))
             {
-            //    jobs = JobDbContext.FindAll();
+                 jobs = context.Jobs
+                    .Include(j => j.Employer)
+                    .ToList();
+
+                foreach (var job in jobs)
+                {
+                    List<JobSkill> jobSkills = context.JobSkills
+                        .Where(js => js.JobId == job.Id)
+                        .Include(js => js.Skill)
+                        .ToList();
+
+                    JobDetailViewModel newDisplayJob = new JobDetailViewModel(job, jobSkills);
+                    displayJobs.Add(newDisplayJob);
+                }
             }
             else
             {
-            //    jobs = JobDbContext.FindByColumnAndValue(searchType, searchTerm);
-            }
-           // ViewBag.columns = ListController.ColumnChoices;
-            //ViewBag.title = "Jobs with " + ListController.ColumnChoices[searchType] + ": " + searchTerm;
-            //ViewBag.jobs = jobs;
+                if (searchType == "employer")
+                {
+                    jobs = context.Jobs
+                        .Include(j => j.Employer)
+                        .Where(j => j.Employer.Name == searchTerm)
+                        .ToList();
 
+                    foreach (Job job in jobs)
+                    {
+                        List<JobSkill> jobSkills = context.JobSkills
+                        .Where(js => js.JobId == job.Id)
+                        .Include(js => js.Skill)
+                        .ToList();
+
+                        JobDetailViewModel newDisplayJob = new JobDetailViewModel(job, jobSkills);
+                        displayJobs.Add(newDisplayJob);
+                    }
+
+                }
+                else if (searchType == "skill")
+                {
+                    List<JobSkill> jobSkills = context.JobSkills
+                        .Where(j => j.Skill.Name == searchTerm)
+                        .Include(j => j.Job)
+                        .ToList();
+
+                    foreach (var job in jobSkills)
+                    {
+                        Job foundJob = context.Jobs
+                            .Include(j => j.Employer)
+                            .Single(j => j.Id == job.JobId);
+
+                        List<JobSkill> displaySkills = context.JobSkills
+                            .Where(js => js.JobId == foundJob.Id)
+                            .Include(js => js.Skill)
+                            .ToList();
+
+                        JobDetailViewModel newDisplayJob = new JobDetailViewModel(foundJob, displaySkills);
+                        displayJobs.Add(newDisplayJob);
+                    }
+                }
+            }
+
+            ViewBag.columns = ListController.ColumnChoices;
+            ViewBag.title = "Jobs with " + ListController.ColumnChoices[searchType] + ": " + searchTerm;
+            ViewBag.jobs = displayJobs;
 
             return View("Index");
         }
